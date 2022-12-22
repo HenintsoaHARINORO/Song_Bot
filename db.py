ec03e0ff376b5442fcc19b22862ea2379a1f030c
HenintsoaHARINORO <48785203+HenintsoaHARINORO@users.noreply.github.com>
2022-12-22 19:15:50 +0300

    Database

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 class DB:
-    def __init__(self, dbname="save.txt"):
+    def __init__(self, dbname="./Database/save.txt"):
         self.dbname = dbname
         self.conn = sqlite3.connect(dbname, check_same_thread=False)
 
@@ -16,7 +16,7 @@ class DB:
         stmt = "INSERT INTO users (message_id,name,song) VALUES (?,?,?)"
         arg = (message_id, name, song)
         clients = pd.read_sql('SELECT * FROM users', self.conn)
-        clients.to_csv('csvdata.csv', index=False)
+        clients.to_csv('./Database/csvdata.csv', index=False)
         self.conn.execute(stmt, arg)
         self.conn.commit()
 
@@ -24,14 +24,8 @@ class DB:
         stmt = "SELECT message_id FROM users where message_id == message_id"
         return [x[0] for x in self.conn.execute(stmt)]
 
-    def delete_item(self, item_text):
-        stmt = "DELETE FROM items WHERE description = (?)"
-        args = (item_text,)
-        self.conn.execute(stmt, args)
-        self.conn.commit()
-
     def get_items(self, message_id):
         clients = pd.read_sql('SELECT * FROM users', self.conn)
-        clients.to_csv('csvdata.csv', index=False)
+        clients.to_csv('./Database/csvdata.csv', index=False)
         stmt = "SELECT song FROM users where message_id == message_id"
         return [x[0] for x in self.conn.execute(stmt)]
